3f8e73fad4fd0b5e14707e7cddb3210b3e070c57
LENINDALLAS <lenindallas.ld@gmail.com>
2023-04-14 22:54:29 +0530

    removed unused code

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -14,6 +14,7 @@ function HomeScreen() {
 
 useEffect(() => {
   dispatch(listProducts())
+// eslint-disable-next-line react-hooks/exhaustive-deps
 } ,[])
 
     return (
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -54,6 +54,7 @@ function OrderScreen(props) {
       }
     }
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, orderId, order, sdkReady]);
 
   const successPaymentHandler = (paymentResult) => {
